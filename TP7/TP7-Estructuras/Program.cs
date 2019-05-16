f2ac760993031422de81478b67cf298aa7390f5e
Ramos-Gabriel <ramogsantana@outlook.com.ar>
2019-05-16 18:19:52 -0300

    TP7 comenzando :D.

---
+++ TP7/TP7-Estructuras/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP7_Estructuras
+{
+    class Program
+    {
+
+        public enum Cargo {Auxiliar,Administrativo, Ingeniero, Especialista, Investigador};
+
+        public struct Personal
+        {
+            public string Nombre, Apellido, FechaNacimiento, EstadoCivil, Genero, FechaIngreso;
+            public double Sueldo;
+            public Cargo cargo;
+        }
+
+
+        static void Main(string[] args)
+        {
+            MENU();
+        }
+
+        static public void MENU()
+        {
+            Console.Write()
+        }
+    }
+}
